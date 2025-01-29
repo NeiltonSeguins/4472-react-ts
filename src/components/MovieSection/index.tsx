356330b02ea5a109225e574a1728f1841a1e9f43
NeiltonSeguins <seguins.neilton@gmailcom>
2025-01-29 15:21:41 -0300

    Vídeo 4.4 - Isolando responsabilidades

--- src/components/MovieSection/index.tsx
+++ src/components/MovieSection/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./MovieSection.module.css";
 import Fieldset from "../Fieldset";
 import InputText from "../InputText";
@@ -6,21 +6,32 @@ import Button from "../Button";
 import { FaSearch } from "react-icons/fa";
 import MovieList from "../MovieList";
 import useFetchMovies from "../../hooks/useFetchMovies";
+import useFilterMovies from "../../hooks/useFilterMovies";
 
 const MovieSection = () => {
   const { movies, error, isLoading } = useFetchMovies();
+  const { searchTerm, handleSearch, setSearchTerm, filteredMovies } =
+    useFilterMovies(movies);
 
   return (
     <main>
       <section className={styles.container}>
         <Fieldset variant="secondary">
-          <InputText placeholder="Buscar filmes..." />
-          <Button variant="icon">
+          <InputText
+            value={searchTerm}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchTerm(event.target.value)
+            }
+            placeholder="Buscar filmes..."
+          />
+          <Button variant="icon" onClick={handleSearch}>
             <FaSearch />
           </Button>
         </Fieldset>
         <h1 className={styles.titulo}>Em cartaz</h1>
-        <MovieList movies={movies} />
+        {isLoading && <p>Carregando filmes...</p>}
+        {error && <p className={styles.error}>{error}</p>}
+        <MovieList movies={filteredMovies} />
       </section>
     </main>
   );
